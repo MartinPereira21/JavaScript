b0fcdac052d814cf256181ed7fc177717de5ec6b
Martin Pereira <93211601+MartinPereira21@users.noreply.github.com>
2021-12-10 13:35:19 +0100

    Add files via upload

---
+++ Tarefa3_Mates/escripito.js
@@ -0,0 +1,20 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+ 	var moito = 9 ;
+    var raiz = Math.sqrt(moito);
+    alert(raiz);
+
+    var uno	= 8.345 ;
+    var dos = 2.446 ;
+    var tres = Math.pow(uno,dos);
+    alert(tres)
+
+    alert(Math.round(Math.PI));
+
+    alert(Math.min(moito,uno,dos));
+
+    function getRandom () {};
+    alert(Math.round(Math.random(0,3)));
